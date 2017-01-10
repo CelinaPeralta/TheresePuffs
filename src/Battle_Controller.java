bf3a8b98b1e0ad1c3319da83b4e9633cc0fd3fbc
CelinaPeralta <celper19@bergen.org>
2017-01-10 11:55:44 -0500

    generated getters and setters for Character class

--- src/Battle_Controller.java
+++ src/Battle_Controller.java
@@ -1,38 +1,42 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-
 /**
  * Created by celinaperalta on 12/29/16.
  */
 
-
 public class Battle_Controller {
 
-    //Possibly make character list
-    final ArrayList<String> VILLAIN_NAMES = new ArrayList<String>();
+	// Possibly make character list
+	final ArrayList<String> VILLAIN_NAMES = new ArrayList<String>();
+	private Random rand = new Random();
 
-    public Battle_Controller() {
+	public Battle_Controller() {
 
-    	VILLAIN_NAMES.add("Cenpai"); 
-    	VILLAIN_NAMES.add("Selena Persalta"); 
-    	VILLAIN_NAMES.add("Sillina Purralta"); 
-    	VILLAIN_NAMES.add("Assorted Cans"); 
-    	VILLAIN_NAMES.add("Commander Algae"); 
-    	VILLAIN_NAMES.add("Matt Bansil");
-    	VILLAIN_NAMES.add("Celili");
-    	VILLAIN_NAMES.add("Celulu");
-    	VILLAIN_NAMES.add("MattyBRaps");
+		VILLAIN_NAMES.add("Cenpai");
+		VILLAIN_NAMES.add("Selena Persalta");
+		VILLAIN_NAMES.add("Sillina Purralta");
+		VILLAIN_NAMES.add("Assorted Cans");
+		VILLAIN_NAMES.add("Commander Algae");
+		VILLAIN_NAMES.add("Matt Bansil");
+		VILLAIN_NAMES.add("Celili");
+		VILLAIN_NAMES.add("Celulu");
+		VILLAIN_NAMES.add("MattyBRaps");
 
-    }
+	}
 
-	Random rand = new Random();
-    public void attack(Character attacker, Character attackee, int dieroll1, int dieroll2, int[] attack) {
-    	
-    }
-    
-    public int dieRoll() {
-    	return rand.nextInt(6)+1;
-    }
+	public void attack(Character attacker, Character attackee, int dieroll1, int dieroll2, int[] attack) {
+
+		int roll1 = dieRoll();
+		int roll2 = dieRoll();
+		System.out.println("Roll 1: " + roll1 + ", Roll 2: " + roll2 + ", Total: " + (roll1 + roll2));
+
+	}
+
+	public int dieRoll() {
+
+		// return some number 1-6
+		return rand.nextInt(6) + 1;
+	}
 
 }
