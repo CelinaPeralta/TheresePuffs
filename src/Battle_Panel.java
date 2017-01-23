531615f0f8193bc72922fb558d92a4f92ba65457
CelinaPeralta <celper19@bergen.org>
2017-01-23 10:28:08 -0500

    how are we going to connect all this

--- src/Battle_Panel.java
+++ src/Battle_Panel.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
+import java.util.Set;
 
 import javax.swing.JPanel;
 import java.awt.FlowLayout;
@@ -16,7 +18,7 @@ public class Battle_Panel extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public Battle_Panel() {
+	public Battle_Panel(Character c) {
 		setBackground(new Color(255, 255, 255));
 
 		setSize(500, 500);
@@ -33,20 +35,23 @@ public class Battle_Panel extends JPanel {
 		controls.setLayout(null);
 		
 		//Pass in attack names
+		//Mr. Isecke would be yelling at me rn
+		String[] current_attacks = (String[]) c.getAttack_list().keySet().toArray();
 		
-		JButton attack1 = new JButton("New Button");
+		
+		JButton attack1 = new JButton(current_attacks[0]);
 		attack1.setBounds(30, 37, 117, 29);
 		controls.add(attack1);
 		
-		JButton attack2 = new JButton("New button");
+		JButton attack2 = new JButton(current_attacks[1]);
 		attack2.setBounds(30, 92, 117, 29);
 		controls.add(attack2);
 		
-		JButton attack3 = new JButton("New button");
+		JButton attack3 = new JButton(current_attacks[2]);
 		attack3.setBounds(360, 37, 117, 29);
 		controls.add(attack3);
 		
-		JButton attack4 = new JButton("New button");
+		JButton attack4 = new JButton(current_attacks[3]);
 		attack4.setBounds(360, 92, 117, 29);
 		controls.add(attack4);
 	}
