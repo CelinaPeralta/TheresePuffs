094709c53cdf8c786ae59b2fa2434d4f7f197be7
CelinaPeralta <celper19@bergen.org>
2017-01-28 18:03:51 -0500

    what

--- src/Battle_Panel.java
+++ src/Battle_Panel.java
@@ -1,23 +1,21 @@
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Map;
-import java.util.Set;
 
-import javax.swing.JPanel;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
-
-import javax.swing.JSplitPane;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 public class Battle_Panel extends JPanel {
 
 	private Character c;
 	private JLabel lblDie, lblSum, lblDie2, lblHitProbability, lblSelectedMove, lblMoveCost, lblDamage;
+	private JPanel player_name, enemy_name;
+	private JButton btnNewButton;
+	private JProgressBar progressBar, progressBar_1;
 	Battle_Controller battle_controller = new Battle_Controller();
 
 	private Character next_villain = battle_controller.getCharacter();
@@ -59,23 +57,41 @@ public class Battle_Panel extends JPanel {
 		panel.add(lblHitProbability);
 
 		// fml how to paint this
-		JPanel player_health_bar = new JPanel();
-		player_health_bar.setBounds(6, 235, 255, 27);
-		display.add(player_health_bar);
-		player_health_bar.setLayout(null);
+		JPanel player_name = new JPanel();
+		player_name.setBounds(6, 235, 255, 27);
+		display.add(player_name);
+		player_name.setLayout(null);
 
 		JLabel label_1 = new JLabel(c.getName());
 		label_1.setBounds(6, 6, 243, 16);
-		player_health_bar.add(label_1);
+		player_name.add(label_1);
 
-		JPanel enemy_health_bar = new JPanel();
-		enemy_health_bar.setBounds(239, 6, 255, 27);
-		display.add(enemy_health_bar);
-		enemy_health_bar.setLayout(null);
+		JPanel enemy_name = new JPanel();
+		enemy_name.setBounds(239, 6, 255, 27);
+		display.add(enemy_name);
+		enemy_name.setLayout(null);
 
 		JLabel label = new JLabel(next_villain.getName());
 		label.setBounds(6, 6, 243, 16);
-		enemy_health_bar.add(label);
+		enemy_name.add(label);
+
+		progressBar = new JProgressBar();
+		progressBar.setBackground(new Color(0, 255, 0));
+		progressBar.setForeground(Color.GREEN);
+		progressBar.setBounds(6, 209, 255, 20);
+		progressBar.setMaximum(c.getMax_health());
+		progressBar.setMinimum(0);
+		display.add(progressBar);
+		progressBar.setValue(c.getMax_health());
+
+		progressBar_1 = new JProgressBar();
+		progressBar_1.setBackground(new Color(0, 255, 0));
+		progressBar_1.setForeground(Color.GREEN);
+		progressBar_1.setBounds(239, 45, 255, 20);
+		progressBar_1.setMaximum(next_villain.getMax_health());
+		progressBar_1.setMinimum(0);
+		display.add(progressBar_1);
+		progressBar_1.setValue(next_villain.getMax_health());
 
 		JPanel controls = new JPanel();
 		controls.setBackground(new Color(245, 245, 245));
@@ -132,21 +148,34 @@ public class Battle_Panel extends JPanel {
 			JButton button = (JButton) e.getSource();
 
 			if (button.getText().equals("Attack!")) {
-				battle_controller.attack(c, next_villain, lblSelectedMove.getText().substring("Selected Move: ".length()));
-				lblDie.setText("Roll 1: " + Integer.toString(battle_controller.getRoll1()));
-				lblDie2.setText("Roll 2: " + Integer.toString(battle_controller.getRoll2()));
+
+				// Attack
+				battle_controller.attack(c, next_villain,
+						lblSelectedMove.getText().substring("Selected Move: ".length()));
+
+				// Set labels
+				lblDie.setText("  Roll 1: " + Integer.toString(battle_controller.getRoll1()));
+				lblDie2.setText("  Roll 2: " + Integer.toString(battle_controller.getRoll2()));
 				lblSum.setText("Sum: " + Integer.toString(battle_controller.getTotal()));
+
+				progressBar.setValue(c.getHealth());
+				progressBar_1.setValue(next_villain.getHealth());
+
 				try {
 					Thread.sleep(50);
 				} catch (InterruptedException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				if (next_villain.getHealth() <= 0)
+				if (next_villain.getHealth() <= 0) {
 					System.out.println("Enemy died what to do now");
+					btnNewButton.setEnabled(false);
+				}
 				battle_controller.attack(next_villain, c, next_villain.getRandomAttack());
-				if (c.getHealth() <= 0)
+				if (c.getHealth() <= 0) {
 					System.out.println("Player died what to do now");
+					btnNewButton.setEnabled(false);
+				}
 			} else {
 				lblSelectedMove.setText("Selected Move: " + button.getText());
 				lblMoveCost.setText("Move Cost: " + c.getAttackValue(button.getText()));
