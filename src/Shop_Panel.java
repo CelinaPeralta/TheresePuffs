fc9e71f843707b9717c950226b92b14470f3a1ea
Anand Shetler <anandshetler@gmail.com>
2017-01-29 18:58:43 -0500

    start gamble

--- src/Shop_Panel.java
+++ src/Shop_Panel.java
@@ -1,6 +1,10 @@
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
@@ -10,41 +14,67 @@ public class Shop_Panel extends JPanel {
 	 * Create the panel.
 	 * @param c 
 	 */
+	
+	JLabel lblDie, lblDie_1, lblDie_2;
+	Character c;
+	
 	public Shop_Panel(Character c) {
+		this.c = c;
 		setSize(500, 500);
 		setLayout(new GridLayout(1, 0, 0, 0));
-		
+
 		JPanel panel = new JPanel();
 		add(panel);
 		panel.setLayout(null);
-		
+
 		JPanel panel_1 = new JPanel();
 		add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JButton btnGamble = new JButton("Gamble");
 		btnGamble.setBounds(66, 410, 117, 29);
+		btnGamble.addActionListener(new ButtonListener());
 		panel_1.add(btnGamble);
-		
-		JLabel lblDie = new JLabel("Die 1:");
+
+		lblDie = new JLabel("Die 1:");
 		lblDie.setBounds(66, 231, 128, 16);
 		panel_1.add(lblDie);
-		
-		JLabel lblDie_1 = new JLabel("Die 2:");
+
+		lblDie_1 = new JLabel("Die 2:");
 		lblDie_1.setBounds(66, 259, 128, 16);
 		panel_1.add(lblDie_1);
-		
-		JLabel lblDie_2 = new JLabel("Die 3:");
+
+		lblDie_2 = new JLabel("Die 3:");
 		lblDie_2.setBounds(66, 290, 128, 16);
 		panel_1.add(lblDie_2);
-		
+
 		JLabel lblPuffs = new JLabel("Puffs:");
 		lblPuffs.setBounds(66, 191, 117, 16);
 		panel_1.add(lblPuffs);
-		
+
 		JLabel lblPuffsWon = new JLabel("Puffs Won:");
 		lblPuffsWon.setBounds(66, 338, 117, 16);
 		panel_1.add(lblPuffsWon);
 	}
+	
+	private void updateLabels(int[] rolls, int puffs_won) {
+		lblDie.setText(String.valueOf(rolls[0]));
+		lblDie_1.setText(String.valueOf(rolls[1]));
+		lblDie_2.setText(String.valueOf(rolls[2]));
+	}
+	
+	public class ButtonListener implements ActionListener {
 
+		public void actionPerformed(ActionEvent e) {
+			Random rand = new Random();
+			int[] rolls = {rand.nextInt(6)+1,rand.nextInt(6)+1,rand.nextInt(6)+1};
+			int puffs_won = 0;
+			Arrays.sort(rolls);
+			if (rolls[0] == rolls[1] && rolls[0] == rolls[2]) puffs_won = 75;
+			else if (rolls[0] == rolls[1]-1 && rolls[1] == rolls[2]-1) puffs_won = 50;
+			else if (rolls[0] == rolls[1] || rolls[1] == rolls[2] || rolls[2] == rolls[0]) puffs_won = 25;
+			c.setPuffs(c.getPuffs()+puffs_won);
+			updateLabels(rolls, puffs_won);
+		}
+	}
 }
